a3565b6512c7218010e5b72f36955428747ffa7c
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-06-04 15:59:53 +0100

    chore(setup): setup base structure for application - use bootstrap CSS framework - setup react-router-dom - create file and folder structure

---
+++ src/components/class/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        Hello World!
+      </div>
+     );
+  }
+}
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+ export const Header = () => {
+   return ( 
+     <div className="container">This is the header!</div>
+    );
+ }
+  
